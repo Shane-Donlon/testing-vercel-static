66ac1acafbabfdc82a008c3d0d8dfec5a5aac301
Shane-Donlon <130906067+Shane-Donlon@users.noreply.github.com>
2025-03-24 13:31:57 +0000

    update package

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   integrations: [qwikdev()],
   adapter: vercel(),
 });
